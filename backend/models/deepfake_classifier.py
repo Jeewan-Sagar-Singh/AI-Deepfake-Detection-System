9f4797efb254644f782e3897b82f7d69572f952d
Jeewan Sagar Singh <jeewansagarsinghtakuli@gmail.com>
2026-06-14 13:56:47 +0530

    Added preprocessing pipeline for classifier

--- backend/models/deepfake_classifier.py
+++ backend/models/deepfake_classifier.py
@@ -1,6 +1,18 @@
+from PIL import Image
+import torchvision.transforms as transforms
+
 def predict_deepfake(face_path):
+
+    image = Image.open(face_path).convert("RGB")
+
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+    ])
+
+    tensor = transform(image)
 
     return {
         "prediction": "FAKE",
-        "confidence": "92%"
+        "confidence": str(tensor.shape)
     }
--- backend/services/deepfake_service.py
+++ backend/services/deepfake_service.py
@@ -1,5 +1,8 @@
 import cv2
+import os
+
 from models.face_detector import detect_faces
+from models.deepfake_classifier import predict_deepfake
 
 def analyze_image(image_path):
 
@@ -9,11 +12,24 @@ def analyze_image(image_path):
 
     face_count = detect_faces(image_path)
 
+    prediction = {
+        "prediction": "NO_FACE",
+        "confidence": "0%"
+    }
+
+    if face_count > 0:
+
+        first_face = os.path.join(
+            "face_crops",
+            "face_0.jpg"
+        )
+
+        prediction = predict_deepfake(first_face)
+
     return {
         "height": height,
         "width": width,
         "channels": channels,
         "faces_detected": face_count,
-        "prediction": "FAKE",
-        "confidence": "92%"
+        **prediction
     }
