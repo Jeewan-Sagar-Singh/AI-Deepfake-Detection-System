def6fd8cdbeadd4fa97187a0d0f7667db5358c1b
Jeewan Sagar Singh <jeewansagarsinghtakuli@gmail.com>
2026-06-14 13:22:44 +0530

    Added video frame extraction and YOLO analysis

--- backend/services/video_service.py
+++ backend/services/video_service.py
@@ -1,4 +1,10 @@
 import cv2
+import os
+from models.face_detector import detect_faces
+
+FRAMES_DIR = "frames"
+
+os.makedirs(FRAMES_DIR, exist_ok=True)
 
 def analyze_video(video_path):
 
@@ -7,23 +13,39 @@ def analyze_video(video_path):
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = cap.get(cv2.CAP_PROP_FPS)
 
-    sampled_frames = 0
+    frame_index = 0
+    saved_frames = 0
+    total_faces = 0
 
     while True:
+
         ret, frame = cap.read()
 
         if not ret:
             break
 
-        sampled_frames += 1
+        if frame_index % 20 == 0:
 
-        if sampled_frames >= 10:
-            break
+            frame_path = os.path.join(
+                FRAMES_DIR,
+                f"frame_{saved_frames}.jpg"
+            )
+
+            cv2.imwrite(frame_path, frame)
+
+            faces = detect_faces(frame_path)
+
+            total_faces += faces
+
+            saved_frames += 1
+
+        frame_index += 1
 
     cap.release()
 
     return {
         "total_frames": total_frames,
         "fps": fps,
-        "sampled_frames": sampled_frames
+        "frames_extracted": saved_frames,
+        "faces_detected": total_faces
     }
